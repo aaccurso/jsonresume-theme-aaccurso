90ca410946a50663ebc5e2e88eafe99a5f806d8e
aaccurso <accurso.alan@gmail.com>
2017-09-10 00:50:11 -0300

    Add volunteering section

--- index.js
+++ index.js
@@ -7,24 +7,32 @@ function render(resumeObject) {
 
 
 	_.each(resumeObject.work, function(w){
-		w.startDateYear = w.startDate.substr(0,7);
+		w.startDate = w.startDate.substr(0,7);
 		if(w.endDate) {
-			w.endDateYear = w.endDate.substr(0,7);
+			w.endDate = w.endDate.substr(0,7);
 		} else { 
-			w.endDateYear = 'Present'
+			w.endDate = 'Present'
+		}
+	});
+	_.each(resumeObject.volunteer, function(w){
+		w.startDate = w.startDate.substr(0,7);
+		if(w.endDate) {
+			w.endDate = w.endDate.substr(0,7);
+		} else { 
+			w.endDate = 'Present'
 		}
 	});
 	_.each(resumeObject.education, function(e){
     if( !e.area || !e.studyType ){
       e.educationDetail = (e.area == null ? '' : e.area) + (e.studyType == null ? '' : e.studyType);
     }  else {
-      e.educationDetail = e.area + ", "+ e.studyType;
+      e.educationDetail = e.area + ' – ' + e.studyType;
     }
-		e.startDateYear = e.startDate.substr(0,7);
+		e.startDate = e.startDate.substr(0,7);
 		if(e.endDate) {
-			e.endDateYear = e.endDate.substr(0,7);
+			e.endDate = e.endDate.substr(0,7);
 		}  else { 
-			e.endDateYear = 'Present'
+			e.endDate = 'Present'
 		}
 	});
 	resumeObject.profiles = {};
